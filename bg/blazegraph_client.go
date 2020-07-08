97b1d5d2115b885f0f5d398088aef8ae22918f63
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-07-07 23:18:40 -0700

    Method for posting new data to Blazegraph

--- bg/blazegraph_client.go
+++ bg/blazegraph_client.go
@@ -18,21 +18,31 @@ func NewBlazegraphClient() *BlazegraphClient {
 	return bc
 }
 
-func (bc *BlazegraphClient) PostSparqlQuery(query string) string {
+func (bc *BlazegraphClient) PostSparqlQuery(resultFormat string, query string) string {
 	request, _ := http.NewRequest("POST", bc.endpoint, strings.NewReader(query))
 	request.Header.Add("Content-Type", "application/sparql-query")
-	request.Header.Add("Accept", "application/json")
+	request.Header.Add("Accept", resultFormat)
 	response, _ := bc.httpClient.Do(request)
 	body, _ := ioutil.ReadAll(response.Body)
 	response.Body.Close()
 	return string(body)
 }
 
-func (bc *BlazegraphClient) GetAllTriples() string {
-	result := bc.PostSparqlQuery(`
-		SELECT ?s ?p ?o
-		WHERE
-		{}
-	`)
+func (bc *BlazegraphClient) PostNewData(data string) string {
+	request, _ := http.NewRequest("POST", bc.endpoint, strings.NewReader(data))
+	request.Header.Add("Content-Type", "application/x-turtle")
+	response, _ := bc.httpClient.Do(request)
+	body, _ := ioutil.ReadAll(response.Body)
+	response.Body.Close()
+	return string(body)
+}
+
+func (bc *BlazegraphClient) GetAllTriplesAsJSON() string {
+	result := bc.PostSparqlQuery(
+		"application/json",
+		`SELECT ?s ?p ?o
+		 WHERE
+		 {}`,
+	)
 	return result
 }
--- bg/blazegraph_client_test.go
+++ bg/blazegraph_client_test.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 )
 
-func ExampleBlazegraphClient_GetAllTriples() {
+func ExampleBlazegraphClient_EmptyStore_GetAllTriplesAsJSON() {
 	bc := NewBlazegraphClient()
-	result := bc.GetAllTriples()
+	result := bc.GetAllTriplesAsJSON()
 	fmt.Println(result)
 	// Output:
 	// {
@@ -17,4 +17,18 @@ func ExampleBlazegraphClient_GetAllTriples() {
 	//     "bindings" : [ { } ]
 	//   }
 	// }
+}
+
+func ExampleBlazegraph_Client_EmptyStore_PostTwoTriples() {
+	bc := NewBlazegraphClient()
+	result := bc.PostNewData(`
+		@prefix ab:    <http://learningsparql.com/ns/addressbook#> .
+		@prefix d:     <http://learningsparql.com/ns/data#> .
+
+		d:y ab:tag "seven" .
+		d:x ab:tag "eight" .
+	`)
+	fmt.Println(result[0:54])
+	// Output:
+	// <?xml version="1.0"?><data modified="2" milliseconds="
 }
