224811c7940ef24f3c08b466ef9cc60f6521195e
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-07-09 14:40:07 -0700

    Cleaned up JSON formatting in tests.

--- bg/blazegraph_client_test.go
+++ bg/blazegraph_client_test.go
@@ -1,7 +1,6 @@
 package bg
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -23,14 +22,43 @@ func TestBlazegraphClient_InsertOneTriple(t *testing.T) {
 	bc := NewBlazegraphClient()
 	bc.deleteAllTriples()
 	bc.PostNewData(`
-	@prefix ab:    <http://learningsparql.com/ns/addressbook#> .
-	@prefix d:     <http://learningsparql.com/ns/data#> .
-
+	@prefix t: <http://tmcphill.net/tags#> .
+	@prefix d: <http://tmcphill.net/data#> .
 	d:y ab:tag "seven" .
 	`)
 
 	queryResult := bc.PostSparqlQuery(
-		`prefix ab: <http://learningsparql.com/ns/addressbook#>
+		`prefix t: <http://tmcphill.net/tags#>
+		 SELECT ?s ?o
+		 WHERE
+		 { ?s t:tag ?o }
+		 `)
+
+	AssertJSONEquals(t,
+		queryResult,
+		`{
+			"head" : { "vars" : [ "s", "o" ] },
+			"results" : {
+			  "bindings" : [ {
+				"s" : { "type" : "uri",     "value" : "http://tmcphill.net/data#y" },
+				"o" : { "type" : "literal", "value" : "seven" }
+			  } ]
+			}
+		  }`)
+}
+
+func TestBlazegraphClient_InsertTwoTriples(t *testing.T) {
+	bc := NewBlazegraphClient()
+	bc.deleteAllTriples()
+	bc.PostNewData(`
+		@prefix t: <http://tmcphill.net/tags#> .
+		@prefix d: <http://tmcphill.net/data#> .
+
+		d:x t:tag "seven" .
+		d:y t:tag "eight" .
+	`)
+	queryResult := bc.PostSparqlQuery(
+		`prefix ab: <http://tmcphill.net/tags#>
 		 SELECT ?s ?o
 		 WHERE
 		 { ?s ab:tag ?o }
@@ -39,35 +67,15 @@ func TestBlazegraphClient_InsertOneTriple(t *testing.T) {
 	AssertJSONEquals(t,
 		queryResult,
 		`{
-			"head" : {
-			  "vars" : [ "s", "o" ]
-			},
+			"head" : { "vars" : [ "s", "o" ] },
 			"results" : {
 			  "bindings" : [ {
-				"s" : {
-				  "type" : "uri",
-				  "value" : "http://learningsparql.com/ns/data#y"
-				},
-				"o" : {
-				  "type" : "literal",
-				  "value" : "seven"
-				}
+				"s" : { "type" : "uri",     "value" : "http://tmcphill.net/data#x" },
+				"o" : { "type" : "literal", "value" : "seven" }
+			}, {
+				"s" : { "type" : "uri",     "value" : "http://tmcphill.net/data#y" },
+				"o" : { "type" : "literal", "value" : "eight" }
 			  } ]
 			}
 		  }`)
-}
-
-func ExampleBlazegraph_Client_EmptyStore_PostTwoTriples() {
-	bc := NewBlazegraphClient()
-	bc.deleteAllTriples()
-	result := bc.PostNewData(`
-		@prefix ab:    <http://learningsparql.com/ns/addressbook#> .
-		@prefix d:     <http://learningsparql.com/ns/data#> .
-
-		d:y ab:tag "seven" .
-		d:x ab:tag "eight" .
-	`)
-	fmt.Println(result[0:54])
-	// Output:
-	// <?xml version="1.0"?><data modified="2" milliseconds="
 }
