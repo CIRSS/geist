9db6618a4d2fa4ee0a5f599d034764c1e5fc44b9
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-07-26 00:43:10 -0700

    Simplified imported  XML

--- go/cmd/blazegraph/blazegraph_cmd_import_test.go
+++ go/cmd/blazegraph/blazegraph_cmd_import_test.go
@@ -112,26 +112,10 @@ func TestBlazegraphCmd_import_two_triples(t *testing.T) {
 		run("blazegraph drop")
 
 		Main.InReader = strings.NewReader(`
-			<rdf:RDF
-				xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-				xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
-				xmlns:sesame="http://www.openrdf.org/schema/sesame#"
-				xmlns:owl="http://www.w3.org/2002/07/owl#"
-				xmlns:xsd="http://www.w3.org/2001/XMLSchema#"
-				xmlns:fn="http://www.w3.org/2005/xpath-functions#"
-				xmlns:foaf="http://xmlns.com/foaf/0.1/"
-				xmlns:dc="http://purl.org/dc/elements/1.1/"
-				xmlns:hint="http://www.bigdata.com/queryHints#"
-				xmlns:bd="http://www.bigdata.com/rdf#"
-				xmlns:bds="http://www.bigdata.com/rdf/search#">
+			<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
 
 			<rdf:Description rdf:about="http://tmcphill.net/data#y">
 				<tag xmlns="http://tmcphill.net/tags#">eight</tag>
-			</rdf:Description>
-
-			<rdf:Description rdf:about="http://tmcphill.net/tags#tag">
-				<rdf:type rdf:resource="http://www.w3.org/1999/02/22-rdf-syntax-ns#Property"/>
-				<rdfs:subPropertyOf rdf:resource="http://tmcphill.net/tags#tag"/>
 			</rdf:Description>
 
 			<rdf:Description rdf:about="http://tmcphill.net/data#x">
