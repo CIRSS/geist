4433e2af5d59dff0f60d70372d0856d3f9f6d4d0
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-07-26 00:28:56 -0700

    Import examples now use n-triples and xml

--- examples/02-blazegraph-examples/01-import/results/S1.sh
+++ examples/02-blazegraph-examples/01-import/results/S1.sh
@@ -1,6 +1,6 @@
 # IMPORT TWO TRIPLES AS N-TRIPLES
 
-blazegraph import --format ttl << END_DATA
+blazegraph import --format nt << END_DATA
 
 	<http://tmcphill.net/data#y> <http://tmcphill.net/tags#tag> "eight" .
 	<http://tmcphill.net/data#x> <http://tmcphill.net/tags#tag> "seven" .
--- examples/02-blazegraph-examples/01-import/run.sh
+++ examples/02-blazegraph-examples/01-import/run.sh
@@ -14,7 +14,7 @@ END_SCRIPT
 
 bash ${RUNNER} S1 "IMPORT TWO TRIPLES AS N-TRIPLES" << END_SCRIPT
 
-blazegraph import --format ttl << END_DATA
+blazegraph import --format nt << END_DATA
 
 	<http://tmcphill.net/data#y> <http://tmcphill.net/tags#tag> "eight" .
 	<http://tmcphill.net/data#x> <http://tmcphill.net/tags#tag> "seven" .
@@ -61,6 +61,47 @@ blazegraph import --format jsonld << END_DATA
             "http://tmcphill.net/tags#tag": "eight"
         }
     ]
+
+END_DATA
+
+blazegraph export --format nt
+
+END_SCRIPT
+
+# *****************************************************************************
+
+bash ${RUNNER} S4 "IMPORT TWO TRIPLES AS RDF-XML" << END_SCRIPT
+
+blazegraph drop
+blazegraph import --format xml << END_DATA
+
+    <rdf:RDF
+        xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+        xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+        xmlns:sesame="http://www.openrdf.org/schema/sesame#"
+        xmlns:owl="http://www.w3.org/2002/07/owl#"
+        xmlns:xsd="http://www.w3.org/2001/XMLSchema#"
+        xmlns:fn="http://www.w3.org/2005/xpath-functions#"
+        xmlns:foaf="http://xmlns.com/foaf/0.1/"
+        xmlns:dc="http://purl.org/dc/elements/1.1/"
+        xmlns:hint="http://www.bigdata.com/queryHints#"
+        xmlns:bd="http://www.bigdata.com/rdf#"
+        xmlns:bds="http://www.bigdata.com/rdf/search#">
+
+    <rdf:Description rdf:about="http://tmcphill.net/data#y">
+        <tag xmlns="http://tmcphill.net/tags#">eight</tag>
+    </rdf:Description>
+
+    <rdf:Description rdf:about="http://tmcphill.net/tags#tag">
+        <rdf:type rdf:resource="http://www.w3.org/1999/02/22-rdf-syntax-ns#Property"/>
+        <rdfs:subPropertyOf rdf:resource="http://tmcphill.net/tags#tag"/>
+    </rdf:Description>
+
+    <rdf:Description rdf:about="http://tmcphill.net/data#x">
+        <tag xmlns="http://tmcphill.net/tags#">seven</tag>
+    </rdf:Description>
+
+    </rdf:RDF>
 
 END_DATA
 
