409cdd399b5b0052231e8d7c332cb92c2b75a1ab
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-07-29 16:15:30 -0700

    Move file path out of data file IDs

--- examples/04-wt-provone-examples/02-prospective-queries/run.sh
+++ examples/04-wt-provone-examples/02-prospective-queries/run.sh
@@ -20,11 +20,14 @@ blazegraph select --format csv << END_QUERY
 
     prefix prov: <http://www.w3.org/ns/prov#>
     prefix provone: <http://purl.dataone.org/provone/2015/01/15/ontology#>
+    prefix wt: <http://wholetale.org/ontology/wt#>
 
-    SELECT ?tale_input_file
+
+    SELECT ?tale_input_file_path
     WHERE {
         ?execution rdf:type provone:Execution .
-        ?execution prov:used ?tale_input_file .
+        ?execution prov:used ?tale_input_file_id .
+        ?tale_input_file_id wt:FilePath ?tale_input_file_path .
     }
 
 END_QUERY
