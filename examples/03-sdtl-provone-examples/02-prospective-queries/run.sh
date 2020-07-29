2368c5ef782ea23ec0971324c7e4b005b40f42d4
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-07-28 19:34:45 -0700

    Query output ports and variables

--- examples/03-sdtl-provone-examples/02-prospective-queries/run.sh
+++ examples/03-sdtl-provone-examples/02-prospective-queries/run.sh
@@ -45,3 +45,25 @@ blazegraph select --format csv << END_QUERY
 END_QUERY
 
 END_SCRIPT
+
+# *****************************************************************************
+
+bash ${RUNNER} Q2 "WHAT ARE THE OUTPUT PORTS AND ASSOCIATED VARIABLES IN THE TRACE?" \
+    << END_SCRIPT
+
+blazegraph select --format csv << END_QUERY
+
+    prefix provone: <http://purl.dataone.org/provone/2015/01/15/ontology#>
+    prefix sdtl: <http://SDTLnamespaceURL#>
+
+    SELECT ?program ?port ?variableName
+    WHERE {
+        ?port rdf:type provone:Port .
+        ?program provone:hasOutPort ?port .
+        ?port sdtl:variable ?variable .
+        ?variable sdtl:variableName  ?variableName .
+    }
+
+END_QUERY
+
+END_SCRIPT
