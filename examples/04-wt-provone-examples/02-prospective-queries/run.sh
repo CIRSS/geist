9a267d2f35100a60d7703f5a799fa9f32ca83045
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-07-30 01:19:10 -0700

    Query run output files

--- examples/04-wt-provone-examples/02-prospective-queries/run.sh
+++ examples/04-wt-provone-examples/02-prospective-queries/run.sh
@@ -33,3 +33,26 @@ blazegraph select --format csv << END_QUERY
 END_QUERY
 
 END_SCRIPT
+
+# *****************************************************************************
+
+bash ${RUNNER} RETROSPECTIVE-1 "WHAT FILES WERE PRODUCED AS OUTPUTS OF THE TALE?" \
+    << END_SCRIPT
+
+blazegraph select --format csv << END_QUERY
+
+    prefix prov: <http://www.w3.org/ns/prov#>
+    prefix provone: <http://purl.dataone.org/provone/2015/01/15/ontology#>
+    prefix wt: <http://wholetale.org/ontology/wt#>
+
+    SELECT ?tale_out_file_path
+    WHERE {
+        ?execution rdf:type provone:Execution .         # find the Tale execution
+        ?execution prov:generated ?tale_output .        # find all outputs of the Tale
+        ?tale_output rdf:type wt:DataFile .             # select just the outputs that are data files
+        ?tale_output wt:FilePath ?tale_out_file_path .  # get the file path for each input data file
+    }
+
+END_QUERY
+
+END_SCRIPT
