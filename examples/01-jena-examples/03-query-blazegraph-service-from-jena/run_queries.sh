2e7e0a1ad54171a8fc79f718a83cf2ba53981740
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-07-31 16:33:44 -0700

    Updated IP address of blazegraph service in Jena tests

--- examples/01-jena-examples/03-query-blazegraph-service-from-jena/run_queries.sh
+++ examples/01-jena-examples/03-query-blazegraph-service-from-jena/run_queries.sh
@@ -4,22 +4,22 @@ run_query='../common/run_query.sh'
 data_file='../data/address-book.jsonld'
 
 bash ${run_query} ${data_file} Q1 "List all triples in blazegraph." << END_QUERY
-PREFIX ab: <http://learningsparql.com/ns/addressbook#> 
-CONSTRUCT 
+PREFIX ab: <http://learningsparql.com/ns/addressbook#>
+CONSTRUCT
 { ?s ?p ?o }
-FROM <http://172.17.0.1:9999/blazegraph/sparql>
-WHERE 
+FROM <http://127.0.0.1:9999/blazegraph/sparql>
+WHERE
 { ?s ?p ?o }
 END_QUERY
 
 bash ${run_query} ${data_file} Q2 "Select all triples in blazegraph." << END_QUERY
-PREFIX ab: <http://learningsparql.com/ns/addressbook#> 
-SELECT ?s ?p ?o 
+PREFIX ab: <http://learningsparql.com/ns/addressbook#>
+SELECT ?s ?p ?o
 WHERE
 {
-    SERVICE <http://172.17.0.2:9999/blazegraph/sparql> 
+    SERVICE <http://127.0.0.1:9999/blazegraph/sparql>
     {
-        SELECT ?s ?p ?o 
+        SELECT ?s ?p ?o
         WHERE {}
     }
 }
