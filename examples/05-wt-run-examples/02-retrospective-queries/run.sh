8c636d0fbf03570db5af971da3fce1837ce9a581
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-08-12 00:14:05 -0700

    First retrospective provenance query of WT run works

--- examples/05-wt-run-examples/02-retrospective-queries/run.sh
+++ examples/05-wt-run-examples/02-retrospective-queries/run.sh
@@ -13,7 +13,7 @@ END_SCRIPT
 
 # *****************************************************************************
 
-bash ${RUNNER} RETROSPECTIVE-1 "WHAT FILES WERE PROVIDED AS INPUT TO THE TALE?" \
+bash ${RUNNER} RETROSPECTIVE-1 "WHAT DATA FILES WERE USED AS INPUT BY THE TALE?" \
     << END_SCRIPT
 
 blazegraph select --format table << END_QUERY
@@ -22,13 +22,17 @@ blazegraph select --format table << END_QUERY
     prefix provone: <http://purl.dataone.org/provone/2015/01/15/ontology#>
     prefix wt: <http://wholetale.org/ontology/wt#>
 
-    SELECT ?tale_input_file_path
+    SELECT DISTINCT ?read_file ?tale_input_file_path
     WHERE {
-        ?execution rdf:type provone:Execution .         # find the Tale execution
-        ?execution prov:used ?tale_input .              # find all inputs to the Tale
-        ?tale_input rdf:type wt:DataFile .              # select just the inputs that are data files
-        ?tale_input wt:FilePath ?tale_input_file_path . # get the file path for each input data file
+        ?run rdf:type wt:TaleRun .                     
+        ?run wt:TaleRunScript ?run_script .         
+        ?run_script_process wt:ExecutionOf ?run_script .             
+        ?run_sub_process wt:ParentProcess ?run_script_process .             
+        ?run_sub_process wt:ReadFile ?read_file .
+        FILTER NOT EXISTS { ?tale_process wt:WroteFile ?read_file . }
+        ?read_file wt:FilePath ?tale_input_file_path .
     }
+    ORDER BY ?tale_input_file_path
 
 END_QUERY
 
