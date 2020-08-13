1641b4a4ccddcf1a0026555b284d1964d70a9557
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-08-13 15:29:22 -0700

    Add example iterating over run output files

--- examples/05-wt-run-examples/03-retrospective-reports/run.sh
+++ examples/05-wt-run-examples/03-retrospective-reports/run.sh
@@ -13,15 +13,16 @@ END_SCRIPT
 
 # *****************************************************************************
 
-bash ${RUNNER} RETROSPECTIVE-1 "WHAT DATA FILES WERE USED AS INPUT BY THE TALE?" \
+bash ${RUNNER} REPORT-1 "WHAT DATA FILES WERE USED AS INPUT BY THE TALE?" \
     << __END_SCRIPT__
 
 blazegraph report << __END_REPORT_TEMPLATE__
-{{ select '''
 
-    PREFIX prov: <http://www.w3.org/ns/prov#>
-    PREFIX provone: <http://purl.dataone.org/provone/2015/01/15/ontology#>
-    PREFIX wt: <http://wholetale.org/ontology/wt#>
+{{ prefix "prov" "http://www.w3.org/ns/prov#" }}
+{{ prefix "provone" "http://purl.dataone.org/provone/2015/01/15/ontology#" }}
+{{ prefix "wt" "http://wholetale.org/ontology/wt#" }}
+
+{{ select '''
 
     SELECT DISTINCT ?tale_input_file_path ?read_file
     WHERE {
@@ -31,12 +32,45 @@ blazegraph report << __END_REPORT_TEMPLATE__
         ?run_sub_process wt:ParentProcess ?run_process .   
         ?run_sub_process wt:ReadFile ?read_file .          
         FILTER NOT EXISTS {                               
-            ?tale_process wt:WroteFile ?read_file . }     
+            ?_ wt:WroteFile ?read_file . }     
         ?read_file wt:FilePath ?tale_input_file_path .
     }
     ORDER BY ?tale_input_file_path
 
 ''' | tabulate }}
+
+__END_REPORT_TEMPLATE__
+
+__END_SCRIPT__
+
+
+bash ${RUNNER} REPORT-2 "WHAT DATA FILES WERE USED AS INPUT BY THE TALE?" \
+    << '__END_SCRIPT__'
+
+blazegraph report << '__END_REPORT_TEMPLATE__'
+
+{{ prefix "prov" "http://www.w3.org/ns/prov#" }}
+{{ prefix "provone" "http://purl.dataone.org/provone/2015/01/15/ontology#" }}
+{{ prefix "wt" "http://wholetale.org/ontology/wt#" }}
+
+{{ range (select '''
+
+    SELECT DISTINCT ?tale_input_file_path
+    WHERE {
+        ?run rdf:type wt:TaleRun .                          
+        ?run wt:TaleRunScript ?run_script .                 
+        ?run_process wt:ExecutionOf ?run_script .               
+        ?run_sub_process wt:ParentProcess ?run_process .   
+        ?run_sub_process wt:ReadFile ?read_file .          
+        FILTER NOT EXISTS {                               
+            ?_ wt:WroteFile ?read_file . }     
+        ?read_file wt:FilePath ?tale_input_file_path .
+    }
+    ORDER BY ?tale_input_file_path
+
+''' | vector) }}
+    {{println .}}
+{{end}}
 
 __END_REPORT_TEMPLATE__
 
