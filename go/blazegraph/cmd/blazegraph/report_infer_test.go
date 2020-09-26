e361c47b650e1b3562ad0ad352787ba4f610aa37
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-09-26 01:07:36 -0700

    Test owl inference using inverseOf

--- go/blazegraph/cmd/blazegraph/report_infer_test.go
+++ go/blazegraph/cmd/blazegraph/report_infer_test.go
@@ -202,3 +202,178 @@ func TestReportInfer_rdfs_subClassOf(t *testing.T) {
 	})
 
 }
+
+func TestReportInfer_rdf_inverseOf(t *testing.T) {
+
+	var outputBuffer strings.Builder
+	Main.OutWriter = &outputBuffer
+	Main.ErrWriter = &outputBuffer
+
+	load := func() {
+		Main.InReader = strings.NewReader(`
+			@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+			@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+			@prefix owl: <http://www.w3.org/2002/07/owl#> .
+			@prefix person: <http://tmcphill.net/ns/person#> .
+			@prefix verb: <http://tmcphill.net/ns/verb#> .
+			@prefix tool: <http://tmcphill.net/ns/tool#> .
+
+			verb:usedBy owl:inverseOf verb:uses .
+
+			person:tim verb:uses tool:mouse .
+			tool:keyboard verb:usedBy person:tim .
+		`)
+		run("blazegraph import --format ttl")
+	}
+
+	report := func() {
+		q := `
+			{{ prefix "rdf" "http://www.w3.org/1999/02/22-rdf-syntax-ns#" }}
+			{{ prefix "verb" "http://tmcphill.net/ns/verb#" }}
+
+			*** Object of verb:uses ***
+
+			{{ select '''
+				SELECT ?usedTool
+				WHERE
+				{ ?person verb:uses ?usedTool }
+				ORDER BY ?usedTool ''' | tabulate }}
+
+			*** Subject of verb:usedBy ***
+
+			{{ select '''
+				SELECT ?usedTool
+				WHERE
+				{ ?usedTool verb:usedBy ?person }
+				ORDER BY ?usedTool ''' | tabulate }}
+		`
+		Main.InReader = strings.NewReader(q)
+		run("blazegraph report")
+	}
+
+	t.Run("infer-none", func(t *testing.T) {
+		outputBuffer.Reset()
+		run("blazegraph destroy --dataset kb")
+		run("blazegraph create --dataset kb --infer none")
+		load()
+		report()
+		util.LineContentsEqual(t, outputBuffer.String(), `
+			*** Object of verb:uses ***
+
+			usedTool
+			================================
+			http://tmcphill.net/ns/tool#mouse
+
+
+			*** Subject of verb:usedBy ***
+
+			usedTool
+			===================================
+			http://tmcphill.net/ns/tool#keyboard
+		`)
+	})
+
+	t.Run("infer-rdfs", func(t *testing.T) {
+		outputBuffer.Reset()
+		run("blazegraph destroy --dataset kb")
+		run("blazegraph create --dataset kb --infer rdfs")
+		load()
+		report()
+		util.LineContentsEqual(t, outputBuffer.String(), `
+			*** Object of verb:uses ***
+
+			usedTool
+			================================
+			http://tmcphill.net/ns/tool#mouse
+
+
+			*** Subject of verb:usedBy ***
+
+			usedTool
+			===================================
+			http://tmcphill.net/ns/tool#keyboard
+		`)
+	})
+
+	t.Run("infer-owl", func(t *testing.T) {
+		outputBuffer.Reset()
+		run("blazegraph destroy --dataset kb")
+		run("blazegraph create --dataset kb --infer owl")
+		load()
+		report()
+		util.LineContentsEqual(t, outputBuffer.String(), `
+			*** Object of verb:uses ***
+
+			usedTool
+			===================================
+			http://tmcphill.net/ns/tool#keyboard
+			http://tmcphill.net/ns/tool#mouse
+
+
+			*** Subject of verb:usedBy ***
+
+			usedTool
+			===================================
+			http://tmcphill.net/ns/tool#keyboard
+			http://tmcphill.net/ns/tool#mouse
+		`)
+	})
+
+}
+
+// func TestReportInfer(t *testing.T) {
+
+// 	var outputBuffer strings.Builder
+// 	Main.OutWriter = &outputBuffer
+// 	Main.ErrWriter = &outputBuffer
+
+// 	load := func() {
+// 		Main.InReader = strings.NewReader(`
+// 			@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+// 			@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+// 			@prefix tm: <http://tmcphill.net/ns/data#> .
+// 		`)
+// 		run("blazegraph import --format ttl")
+// 	}
+
+// 	report := func() {
+// 		q := `
+// 			{{ prefix "rdf" "http://www.w3.org/1999/02/22-rdf-syntax-ns#" }}
+// 			{{ prefix "tm" "http://tmcphill.net/ns/data#" }}
+
+// 		`
+// 		Main.InReader = strings.NewReader(q)
+// 		run("blazegraph report")
+// 	}
+
+// 	t.Run("infer-none", func(t *testing.T) {
+// 		outputBuffer.Reset()
+// 		run("blazegraph destroy --dataset kb")
+// 		run("blazegraph create --dataset kb --infer none")
+// 		load()
+// 		report()
+// 		util.LineContentsEqual(t, outputBuffer.String(), `
+// 		`)
+// 	})
+
+// 	t.Run("infer-rdfs", func(t *testing.T) {
+// 		outputBuffer.Reset()
+// 		run("blazegraph destroy --dataset kb")
+// 		run("blazegraph create --dataset kb --infer rdfs")
+// 		load()
+// 		report()
+// 		util.LineContentsEqual(t, outputBuffer.String(), `
+// 		`)
+// 	})
+
+// 	t.Run("infer-owl", func(t *testing.T) {
+// 		outputBuffer.Reset()
+// 		run("blazegraph destroy --dataset kb")
+// 		run("blazegraph create --dataset kb --infer owl")
+// 		load()
+// 		report()
+// 		util.LineContentsEqual(t, outputBuffer.String(), `
+// 		`)
+// 	})
+
+// }
