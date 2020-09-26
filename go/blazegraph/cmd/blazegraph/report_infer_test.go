98ba3d08251ab0febb700b28a195c9d855dc8ee7
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-09-25 22:47:53 -0700

    Include types in test results

--- go/blazegraph/cmd/blazegraph/report_infer_test.go
+++ go/blazegraph/cmd/blazegraph/report_infer_test.go
@@ -38,6 +38,12 @@ func TestReportInfer_rdfs_subClassOf(t *testing.T) {
 			{{ prefix "tm" "http://tmcphill.net/ns/data#" }}
 
 			{{ select '''
+				SELECT ?type
+				WHERE
+				{ ?type rdf:type rdfs:Class }
+				ORDER BY ?type ''' | tabulate }}
+
+			{{ select '''
 				SELECT ?a
 				WHERE
 				{ ?a rdf:type tm:typea }
@@ -66,6 +72,10 @@ func TestReportInfer_rdfs_subClassOf(t *testing.T) {
 		load()
 		report()
 		util.LineContentsEqual(t, outputBuffer.String(), `
+			type
+			===
+
+
 			a
 			=============================
             http://tmcphill.net/ns/data#a1
@@ -91,6 +101,26 @@ func TestReportInfer_rdfs_subClassOf(t *testing.T) {
 		load()
 		report()
 		util.LineContentsEqual(t, outputBuffer.String(), `
+			type
+            ===============================================================
+            http://tmcphill.net/ns/data#typea
+            http://tmcphill.net/ns/data#typeb
+            http://tmcphill.net/ns/data#typec
+            http://www.w3.org/1999/02/22-rdf-syntax-ns#Alt
+            http://www.w3.org/1999/02/22-rdf-syntax-ns#Bag
+            http://www.w3.org/1999/02/22-rdf-syntax-ns#List
+            http://www.w3.org/1999/02/22-rdf-syntax-ns#Property
+            http://www.w3.org/1999/02/22-rdf-syntax-ns#Seq
+            http://www.w3.org/1999/02/22-rdf-syntax-ns#Statement
+            http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral
+            http://www.w3.org/2000/01/rdf-schema#Class
+            http://www.w3.org/2000/01/rdf-schema#Container
+            http://www.w3.org/2000/01/rdf-schema#ContainerMembershipProperty
+            http://www.w3.org/2000/01/rdf-schema#Datatype
+            http://www.w3.org/2000/01/rdf-schema#Literal
+            http://www.w3.org/2000/01/rdf-schema#Resource
+
+
 			a
 			=============================
 			http://tmcphill.net/ns/data#a1
@@ -122,6 +152,31 @@ func TestReportInfer_rdfs_subClassOf(t *testing.T) {
 		load()
 		report()
 		util.LineContentsEqual(t, outputBuffer.String(), `
+			type
+			===============================================================
+			http://tmcphill.net/ns/data#typea
+			http://tmcphill.net/ns/data#typeb
+			http://tmcphill.net/ns/data#typec
+			http://www.w3.org/1999/02/22-rdf-syntax-ns#Alt
+			http://www.w3.org/1999/02/22-rdf-syntax-ns#Bag
+			http://www.w3.org/1999/02/22-rdf-syntax-ns#List
+			http://www.w3.org/1999/02/22-rdf-syntax-ns#Property
+			http://www.w3.org/1999/02/22-rdf-syntax-ns#Seq
+			http://www.w3.org/1999/02/22-rdf-syntax-ns#Statement
+			http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral
+			http://www.w3.org/2000/01/rdf-schema#Class
+			http://www.w3.org/2000/01/rdf-schema#Container
+			http://www.w3.org/2000/01/rdf-schema#ContainerMembershipProperty
+			http://www.w3.org/2000/01/rdf-schema#Datatype
+			http://www.w3.org/2000/01/rdf-schema#Literal
+			http://www.w3.org/2000/01/rdf-schema#Resource
+			http://www.w3.org/2002/07/owl#Class
+			http://www.w3.org/2002/07/owl#DatatypeProperty
+			http://www.w3.org/2002/07/owl#ObjectProperty
+			http://www.w3.org/2002/07/owl#Restriction
+			http://www.w3.org/2002/07/owl#TransitiveProperty
+
+
 			a
 			=============================
 			http://tmcphill.net/ns/data#a1
