a9ad268a4410a49ca9666573008440e7566c9955
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-10-05 16:54:34 -0700

    Test demonstrating Blazegraph rdfs and owl inference behavior when rdf:type of an object does not match given rdfs:domain or rdfs:range of predicate.

--- go/blazegraph/cmd/blazegraph/report_infer_test.go
+++ go/blazegraph/cmd/blazegraph/report_infer_test.go
@@ -318,6 +318,222 @@ func TestReportInfer_rdf_inverseOf(t *testing.T) {
 			http://tmcphill.net/ns/tool#mouse
 		`)
 	})
+}
+
+func TestReportInfer_rdfs_domain_range(t *testing.T) {
+
+	var outputBuffer strings.Builder
+	Main.OutWriter = &outputBuffer
+	Main.ErrWriter = &outputBuffer
+
+	load := func() {
+		Main.InReader = strings.NewReader(`
+			@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+			@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+			@prefix owl: <http://www.w3.org/2002/07/owl#> .
+			@prefix tm: <http://tmcphill.net/ns#> .
+			@prefix person: <http://tmcphill.net/ns/person#> .
+			@prefix verb: <http://tmcphill.net/ns/verb#> .
+			@prefix tool: <http://tmcphill.net/ns/tool#> .
+
+			verb:uses rdfs:domain tm:Person .
+			verb:uses rdfs:range tm:Tool .
+			verb:usedBy owl:inverseOf verb:uses .
+
+			person:tim rdf:type tm:Person .
+			tool:keyboard rdf:type tm:Tool .
+
+			person:tim verb:uses tool:mouse .
+			tool:keyboard verb:usedBy person:tim .
+
+			tool:mouse verb:uses person:tim .
+		`)
+		run("blazegraph import --format ttl")
+	}
+
+	report := func() {
+		q := `
+			{{ prefix "rdf" "http://www.w3.org/1999/02/22-rdf-syntax-ns#" }}
+			{{ prefix "tm" "http://tmcphill.net/ns/data#" }}
+			{{ prefix "verb" "http://tmcphill.net/ns/verb#" }}
+			{{ prefix "tool" "http://tmcphill.net/ns/tool#" }}
+
+			*** Statements about verb:uses ***
+			{{ select '''
+				SELECT ?p ?o
+				WHERE
+				{ verb:uses ?p ?o }
+				ORDER BY ?p ?o ''' | tabulate }}
+
+			*** Statements about tool:keyboard ***
+			{{ select '''
+				SELECT ?p ?o
+				WHERE
+				{ tool:keyboard ?p ?o }
+				ORDER BY ?p ?o ''' | tabulate }}
+
+			*** Statements about tool:mouse ***
+			{{ select '''
+				SELECT ?p ?o
+				WHERE
+				{ tool:mouse ?p ?o }
+				ORDER BY ?p ?o ''' | tabulate }}
+
+			*** What verb:uses what ***
+			{{ select '''
+				SELECT ?s ?o
+				WHERE
+				{ ?s verb:uses ?o }
+				ORDER BY ?s ?o ''' | tabulate }}
+
+			*** What is verb:usedBy what ***
+			{{ select '''
+				SELECT ?s ?o
+				WHERE
+				{ ?s verb:usedBy ?o }
+				ORDER BY ?s ?o ''' | tabulate }}
+			`
+		Main.InReader = strings.NewReader(q)
+		run("blazegraph report")
+	}
+
+	t.Run("infer-none", func(t *testing.T) {
+		outputBuffer.Reset()
+		run("blazegraph destroy --dataset kb")
+		run("blazegraph create --dataset kb --infer none")
+		load()
+		report()
+		util.LineContentsEqual(t, outputBuffer.String(), `
+
+			*** Statements about verb:uses ***
+			p                                           | o
+			===========================================================================
+			http://www.w3.org/2000/01/rdf-schema#domain | http://tmcphill.net/ns#Person
+			http://www.w3.org/2000/01/rdf-schema#range  | http://tmcphill.net/ns#Tool
+
+
+			*** Statements about tool:keyboard ***
+			p                                               | o
+			=============================================================================
+			http://tmcphill.net/ns/verb#usedBy              | http://tmcphill.net/ns/person#tim
+			http://www.w3.org/1999/02/22-rdf-syntax-ns#type | http://tmcphill.net/ns#Tool
+
+
+			*** Statements about tool:mouse ***
+			p                                | o
+			====================================================================
+			http://tmcphill.net/ns/verb#uses | http://tmcphill.net/ns/person#tim
+
+
+			*** What verb:uses what ***
+			s                                 | o
+			=====================================================================
+			http://tmcphill.net/ns/person#tim | http://tmcphill.net/ns/tool#mouse
+			http://tmcphill.net/ns/tool#mouse | http://tmcphill.net/ns/person#tim
+
+
+			*** What is verb:usedBy what ***
+			s                                    | o
+			========================================================================
+			http://tmcphill.net/ns/tool#keyboard | http://tmcphill.net/ns/person#tim
+		`)
+	})
+
+	t.Run("infer-rdfs", func(t *testing.T) {
+		outputBuffer.Reset()
+		run("blazegraph destroy --dataset kb")
+		run("blazegraph create --dataset kb --infer rdfs")
+		load()
+		report()
+		util.LineContentsEqual(t, outputBuffer.String(), `
+
+			*** Statements about verb:uses ***
+			p                                                  | o
+            =====================================================================================================
+            http://www.w3.org/1999/02/22-rdf-syntax-ns#type    | http://www.w3.org/1999/02/22-rdf-syntax-ns#Property
+            http://www.w3.org/2000/01/rdf-schema#domain        | http://tmcphill.net/ns#Person
+            http://www.w3.org/2000/01/rdf-schema#range         | http://tmcphill.net/ns#Tool
+            http://www.w3.org/2000/01/rdf-schema#subPropertyOf | http://tmcphill.net/ns/verb#uses
+
+
+            *** Statements about tool:keyboard ***
+            p                                               | o
+            =============================================================================
+            http://tmcphill.net/ns/verb#usedBy              | http://tmcphill.net/ns/person#tim
+            http://www.w3.org/1999/02/22-rdf-syntax-ns#type | http://tmcphill.net/ns#Tool
+
+
+            *** Statements about tool:mouse ***
+            p                                               | o
+            ===============================================================================
+            http://tmcphill.net/ns/verb#uses                | http://tmcphill.net/ns/person#tim
+            http://www.w3.org/1999/02/22-rdf-syntax-ns#type | http://tmcphill.net/ns#Person
+            http://www.w3.org/1999/02/22-rdf-syntax-ns#type | http://tmcphill.net/ns#Tool
+
+
+            *** What verb:uses what ***
+            s                                 | o
+            =====================================================================
+            http://tmcphill.net/ns/person#tim | http://tmcphill.net/ns/tool#mouse
+            http://tmcphill.net/ns/tool#mouse | http://tmcphill.net/ns/person#tim
+
+
+            *** What is verb:usedBy what ***
+            s                                    | o
+            ========================================================================
+            http://tmcphill.net/ns/tool#keyboard | http://tmcphill.net/ns/person#tim
+        `)
+	})
+
+	t.Run("infer-owl", func(t *testing.T) {
+		outputBuffer.Reset()
+		run("blazegraph destroy --dataset kb")
+		run("blazegraph create --dataset kb --infer owl")
+		load()
+		report()
+		util.LineContentsEqual(t, outputBuffer.String(), `
+			*** Statements about verb:uses ***
+			p                                                  | o
+			=====================================================================================================
+			http://www.w3.org/1999/02/22-rdf-syntax-ns#type    | http://www.w3.org/1999/02/22-rdf-syntax-ns#Property
+			http://www.w3.org/2000/01/rdf-schema#domain        | http://tmcphill.net/ns#Person
+			http://www.w3.org/2000/01/rdf-schema#range         | http://tmcphill.net/ns#Tool
+			http://www.w3.org/2000/01/rdf-schema#subPropertyOf | http://tmcphill.net/ns/verb#uses
+			http://www.w3.org/2002/07/owl#inverseOf            | http://tmcphill.net/ns/verb#usedBy
+
+
+			*** Statements about tool:keyboard ***
+			p                                               | o
+			=============================================================================
+			http://tmcphill.net/ns/verb#usedBy              | http://tmcphill.net/ns/person#tim
+			http://www.w3.org/1999/02/22-rdf-syntax-ns#type | http://tmcphill.net/ns#Tool
+
+
+			*** Statements about tool:mouse ***
+			p                                               | o
+			===============================================================================
+			http://tmcphill.net/ns/verb#usedBy              | http://tmcphill.net/ns/person#tim
+			http://tmcphill.net/ns/verb#uses                | http://tmcphill.net/ns/person#tim
+			http://www.w3.org/1999/02/22-rdf-syntax-ns#type | http://tmcphill.net/ns#Person
+			http://www.w3.org/1999/02/22-rdf-syntax-ns#type | http://tmcphill.net/ns#Tool
+
+
+			*** What verb:uses what ***
+			s                                 | o
+			========================================================================
+			http://tmcphill.net/ns/person#tim | http://tmcphill.net/ns/tool#keyboard
+			http://tmcphill.net/ns/person#tim | http://tmcphill.net/ns/tool#mouse
+			http://tmcphill.net/ns/tool#mouse | http://tmcphill.net/ns/person#tim
+
+
+			*** What is verb:usedBy what ***
+			s                                    | o
+			========================================================================
+			http://tmcphill.net/ns/person#tim    | http://tmcphill.net/ns/tool#mouse
+			http://tmcphill.net/ns/tool#keyboard | http://tmcphill.net/ns/person#tim
+			http://tmcphill.net/ns/tool#mouse    | http://tmcphill.net/ns/person#tim
+		`)
+	})
 
 }
 
