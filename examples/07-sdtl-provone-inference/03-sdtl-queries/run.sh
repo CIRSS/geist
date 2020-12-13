6014afd31f5d9e80445679a59e06834a33d77b75
Timothy McPhillips <tmcphillips@absoluteflow.com>
2020-12-12 20:01:52 -0800

    Query variable loaded or saved by script

--- examples/07-sdtl-provone-inference/03-sdtl-queries/run.sh
+++ examples/07-sdtl-provone-inference/03-sdtl-queries/run.sh
@@ -4,13 +4,15 @@ RUNNER='../../common/run_script_example.sh'
 
 # *****************************************************************************
 
-bash ${RUNNER} SETUP "IMPORT PROVONE TRACE" << END_SCRIPT
+bash ${RUNNER} SETUP "IMPORT SDTL" << END_SCRIPT
 
 blazegraph destroy --dataset kb
 blazegraph create --dataset kb
 blazegraph import --format jsonld --file ../data/compute-sdtl.jsonld
 
 END_SCRIPT
+
+
 
 # *****************************************************************************
 
@@ -22,10 +24,98 @@ blazegraph select --format table << __END_QUERY__
 
     SELECT DISTINCT ?command ?sourcetext
     WHERE {
-        ?program rdf:type sdtl:Program .            # Identify the SDTL program.
+        ?program rdf:type sdtl:Program .
         ?program sdtl:Commands ?command .
         ?command sdtl:SourceInformation ?sourceinfo .
         ?sourceinfo sdtl:originalSourceText ?sourcetext .
+    }
+
+__END_QUERY__
+
+END_SCRIPT
+
+
+
+bash ${RUNNER} Q2 "WHAT DATA FILES ARE LOADED BY THE SCRIPT?" << END_SCRIPT
+
+blazegraph select --format table << __END_QUERY__
+
+    PREFIX sdtl: <https://rdf-vocabulary.ddialliance.org/sdtl#>
+
+    SELECT DISTINCT ?filename ?command ?sourcetext
+    WHERE {
+        ?program rdf:type sdtl:Program .
+        ?program sdtl:Commands ?command .
+        ?command rdf:type sdtl:Load .
+        ?command sdtl:FileName ?filename .
+        ?command sdtl:SourceInformation ?sourceinfo .
+        ?sourceinfo sdtl:originalSourceText ?sourcetext .
+    }
+
+__END_QUERY__
+
+END_SCRIPT
+
+
+
+bash ${RUNNER} Q3 "WHAT DATA FILES ARE SAVED BY THE SCRIPT?" << END_SCRIPT
+
+blazegraph select --format table << __END_QUERY__
+
+    PREFIX sdtl: <https://rdf-vocabulary.ddialliance.org/sdtl#>
+
+    SELECT DISTINCT ?filename ?command ?sourcetext
+    WHERE {
+        ?program rdf:type sdtl:Program .
+        ?program sdtl:Commands ?command .
+        ?command rdf:type sdtl:Save .
+        ?command sdtl:FileName ?filename .
+        ?command sdtl:SourceInformation ?sourceinfo .
+        ?sourceinfo sdtl:originalSourceText ?sourcetext .
+    }
+
+__END_QUERY__
+
+END_SCRIPT
+
+
+
+bash ${RUNNER} Q4 "WHAT VARIABLES ARE LOADED BY THE SCRIPT?" << END_SCRIPT
+
+blazegraph select --format table << __END_QUERY__
+
+    PREFIX sdtl: <https://rdf-vocabulary.ddialliance.org/sdtl#>
+
+    SELECT DISTINCT ?dataframe ?variable
+    WHERE {
+        ?program rdf:type sdtl:Program .
+        ?program sdtl:Commands ?command .
+        ?command rdf:type sdtl:Load .
+        ?command sdtl:ProducesDataframe ?dataframedesc .
+        ?dataframedesc sdtl:DataframeName ?dataframe .
+        ?dataframedesc sdtl:VariableInventory ?variable .
+    }
+
+__END_QUERY__
+
+END_SCRIPT
+
+
+
+bash ${RUNNER} Q5 "WHAT VARIABLES ARE SAVED BY THE SCRIPT?" << END_SCRIPT
+
+blazegraph select --format table << __END_QUERY__
+
+    PREFIX sdtl: <https://rdf-vocabulary.ddialliance.org/sdtl#>
+
+    SELECT DISTINCT ?dataframe ?variable
+    WHERE {
+        ?program rdf:type sdtl:Program .
+        ?program sdtl:Commands ?command .
+        ?command rdf:type sdtl:Save .
+        ?command sdtl:ProducesDataframe ?dataframedesc .
+        ?dataframedesc sdtl:DataframeName ?dataframe .
+        ?dataframedesc sdtl:VariableInventory ?variable .
     }
 
 __END_QUERY__
