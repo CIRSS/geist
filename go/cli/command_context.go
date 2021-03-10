ebc540b6dbb5d787648605ccdf00d0f259be7c2b
Timothy McPhillips <tmcphillips@absoluteflow.com>
2021-03-09 21:04:09 -0800

    Inject writer into help methods

--- go/cli/command_context.go
+++ go/cli/command_context.go
@@ -9,15 +9,6 @@ import (
 	"strings"
 )
 
-type errorMessageWriterStruct struct {
-	errorStream io.Writer
-}
-
-func (emw errorMessageWriterStruct) Write(p []byte) (n int, err error) {
-	fmt.Fprintln(emw.errorStream)
-	return emw.errorStream.Write(p)
-}
-
 type NullWriter struct {
 	w io.Writer
 }
@@ -27,25 +18,24 @@ func (nw NullWriter) Write(p []byte) (n int, err error) {
 }
 
 type CommandContext struct {
-	programContext     *ProgramContext
-	commands           *CommandSet
-	Descriptor         *CommandDescriptor
-	Args               []string
-	Flags              *flag.FlagSet
-	Quiet              *bool
-	InReader           io.Reader
-	OutWriter          io.Writer
-	ErrWriter          io.Writer
-	ErrorMessageWriter errorMessageWriterStruct
-	Properties         map[string]interface{}
-	Providers          map[string]func(cc *CommandContext) interface{}
+	programContext *ProgramContext
+	Commands       *CommandSet
+	Descriptor     *CommandDescriptor
+	Args           []string
+	Flags          *flag.FlagSet
+	Quiet          *bool
+	InReader       io.Reader
+	OutWriter      io.Writer
+	ErrWriter      io.Writer
+	Properties     map[string]interface{}
+	Providers      map[string]func(cc *CommandContext) interface{}
 }
 
 func NewCommandContext(pc *ProgramContext, commands *CommandSet) (cc *CommandContext) {
 
 	cc = new(CommandContext)
 	cc.programContext = pc
-	cc.commands = commands
+	cc.Commands = commands
 
 	cc.Flags = pc.InitFlagSet()
 	cc.Flags.Usage = func() {}
@@ -56,7 +46,6 @@ func NewCommandContext(pc *ProgramContext, commands *CommandSet) (cc *CommandCon
 
 	cc.Properties = make(map[string]interface{})
 	cc.Providers = make(map[string]func(cc *CommandContext) interface{})
-	cc.ErrorMessageWriter.errorStream = cc.ErrWriter
 
 	cc.Quiet = cc.Flags.Bool("quiet", false, "Discard normal command output")
 
@@ -68,32 +57,25 @@ func (cc *CommandContext) AddProvider(resource string, f func(cc *CommandContext
 }
 
 func (cc *CommandContext) Resource(resourceName string) (resource interface{}) {
-
 	provider, exists := cc.Providers[resourceName]
 	if !exists {
 		panic("No resource provider for " + resourceName)
 	}
-
 	resource = provider(cc)
-
 	return
 }
 
-func (cc *CommandContext) Lookup(commandName string) (*CommandDescriptor, bool) {
-	return cc.commands.Lookup(commandName)
-}
-
-func (cc *CommandContext) ShowHelpIfRequested() bool {
+func (cc *CommandContext) ShowHelpIfRequested(w io.Writer) bool {
 	if len(cc.Args) > 1 && cc.Args[1] == "help" {
-		cc.ShowCommandDescription()
-		cc.ShowCommandUsage(cc.OutWriter)
+		cc.ShowCommandDescription(w)
+		cc.ShowCommandUsage(w)
 		return true
 	}
 	return false
 }
 
-func (cc *CommandContext) ShowCommandDescription() {
-	fmt.Fprintf(cc.OutWriter, "\n%s %s: %s\n",
+func (cc *CommandContext) ShowCommandDescription(w io.Writer) {
+	fmt.Fprintf(w, "\n%s %s: %s\n",
 		cc.programContext.Name, cc.Descriptor.Name, cc.Descriptor.Description)
 }
 
@@ -105,18 +87,18 @@ func (cc *CommandContext) ShowCommandUsage(w io.Writer) {
 	fmt.Fprintln(w)
 }
 
-func (cc *CommandContext) ShowProgramUsage() {
-	fmt.Fprintf(cc.OutWriter, "usage: %s <command> [<flags>]\n\n", cc.programContext.Name)
+func (cc *CommandContext) ShowProgramUsage(w io.Writer) {
+	fmt.Fprintf(w, "usage: %s <command> [<flags>]\n\n", cc.programContext.Name)
 }
 
-func (cc *CommandContext) ShowProgramCommands() {
-	fmt.Fprint(cc.OutWriter, "commands:\n")
-	for _, sc := range cc.commands.commandList {
+func (cc *CommandContext) ShowProgramCommands(w io.Writer) {
+	fmt.Fprint(w, "commands:\n")
+	for _, sc := range cc.Commands.commandList {
 		fmt.Fprintf(cc.OutWriter, "  %-7s  - %s\n", sc.Name, sc.Summary)
 	}
-	fmt.Fprint(cc.OutWriter, "\nflags:\n")
+	fmt.Fprint(w, "\nflags:\n")
 	cc.Flags.PrintDefaults()
-	fmt.Fprintf(cc.OutWriter,
+	fmt.Fprintf(w,
 		"\nSee '%s help <command>' for help with one of the above commands.\n\n",
 		cc.programContext.Name)
 	return
@@ -145,7 +127,7 @@ func (cc *CommandContext) ParseCommandFlags() (err error) {
 
 func (cc *CommandContext) ParseFlags() (helpShown bool, err error) {
 
-	if cc.ShowHelpIfRequested() {
+	if cc.ShowHelpIfRequested(cc.OutWriter) {
 		helpShown = true
 		return
 	}
@@ -171,20 +153,20 @@ func (cc *CommandContext) InvokeCommand(args []string) {
 	if len(args) < 2 {
 		fmt.Fprintf(cc.programContext.ErrWriter, "%s: no command given\n\n",
 			cc.programContext.Name)
-		cc.ShowProgramUsage()
-		cc.ShowProgramCommands()
+		cc.ShowProgramUsage(cc.OutWriter)
+		cc.ShowProgramCommands(cc.OutWriter)
 		cc.programContext.ExitIfNonzero(1)
 		return
 	}
 
 	commandName := args[1]
-	descriptor, exists := cc.commands.Lookup(commandName)
+	descriptor, exists := cc.Commands.Lookup(commandName)
 	cc.Descriptor = descriptor
 	if !exists {
 		fmt.Fprintf(cc.programContext.ErrWriter, "%s: unrecognized command: %s\n\n",
 			cc.programContext.Name, commandName)
-		cc.ShowProgramUsage()
-		cc.ShowProgramCommands()
+		cc.ShowProgramUsage(cc.OutWriter)
+		cc.ShowProgramCommands(cc.OutWriter)
 		cc.programContext.ExitIfNonzero(1)
 		return
 	}
@@ -208,23 +190,23 @@ func (cc *CommandContext) InvokeCommand(args []string) {
 func Help(cc *CommandContext) (err error) {
 	if len(cc.Args) < 2 {
 		fmt.Fprintln(cc.OutWriter)
-		cc.ShowProgramUsage()
-		cc.ShowProgramCommands()
+		cc.ShowProgramUsage(cc.OutWriter)
+		cc.ShowProgramCommands(cc.OutWriter)
 		return
 	}
 	commandName := cc.Args[1]
 	if commandName == "help" {
 		return
 	}
-	if c, exists := cc.Lookup(commandName); exists {
+	if c, exists := cc.Commands.Lookup(commandName); exists {
 		cc.Descriptor = c
 		cc.Args = []string{commandName, "help"}
 		c.Handler(cc)
 	} else {
 		fmt.Fprintf(cc.ErrWriter, "%s help: unrecognized %s command: %s\n\n",
 			cc.programContext.Name, cc.programContext.Name, commandName)
-		cc.ShowProgramUsage()
-		cc.ShowProgramCommands()
+		cc.ShowProgramUsage(cc.OutWriter)
+		cc.ShowProgramCommands(cc.OutWriter)
 		err = NewCLIError(nil)
 	}
 	return
