e69aa9fccf9dac3422f53aab30ba39a0c2a1415a
Timothy McPhillips <tmcphillips@absoluteflow.com>
2021-03-13 15:29:26 -0800

    Move definition of blazegraph command context to blazegraph package

--- go/blazegraph/cmd/blazegraph/blazegraph_cmd.go
+++ go/blazegraph/cmd/blazegraph/blazegraph_cmd.go
@@ -14,45 +14,6 @@ func init() {
 }
 
 func main() {
-
-	commands := cli.NewCommandSet([]cli.CommandDescriptor{
-		{"create", blazegraph.Create, "Create a new RDF dataset",
-			"Creates a new RDF dataset and corresponding Blazegraph namespace."},
-		{"destroy", blazegraph.Destroy, "Delete an RDF dataset",
-			"Deletes an RDF dataset and corresponding Blazegraph namespace, all RDF graphs\n" +
-				"in the dataset, and all triples in each of those graphs."},
-		{"export", blazegraph.Export, "Export contents of a dataset",
-			"Exports all triples in an RDF dataset in the requested format."},
-		{"help", cli.Help, "Show help", ""},
-		{"import", blazegraph.Import, "Import data into a dataset",
-			"Imports triples in the specified format into an RDF dataset."},
-		{"list", blazegraph.List, "List RDF datasets",
-			"Lists the names of the RDF datasets in the Blazegraph instance."},
-		{"query", blazegraph.Query, "Perform a SPARQL query on a dataset",
-			"Performs a SPARQL query on the identified RDF dataset."},
-		{"report", blazegraph.Report, "Expand a report using a dataset",
-			"Expands the provided report template using the identified RDF dataset."},
-		{"status", blazegraph.Status, "Check the status of the Blazegraph instance",
-			"Requests the status of the Blazegraph instance, optionally waiting until\n" +
-				"the instance is fully running. Returns status in JSON format."},
-	})
-
-	cc := Main.NewCommandContext(commands)
-	cc.AddProvider("BlazegraphClient", getBlazegraphClient)
-
-	cc.Flags.String("instance", blazegraph.DefaultUrl, "`URL` of Blazegraph instance")
-
+	cc := blazegraph.NewBlazeCommandContext(Main)
 	cc.InvokeCommand(os.Args)
-}
-
-func getBlazegraphClient(cc *cli.CommandContext) (bc interface{}) {
-	bcc, exists := cc.Properties["blazegraph_client"]
-	if exists {
-		bc = bcc.(*blazegraph.BlazegraphClient)
-	} else {
-		instanceFlag := cc.Flags.Lookup("instance").Value.String()
-		bc = blazegraph.NewBlazegraphClient(instanceFlag)
-		cc.Properties["blazegraph_client"] = bc
-	}
-	return
 }
