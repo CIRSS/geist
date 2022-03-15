7e39b244cf58efe7a2d079667fd996f555d24210
Timothy McPhillips <tmcphillips@absoluteflow.com>
2022-03-14 22:45:02 -0700

    Get  examples working

---
+++ .repro/exported/setup-demo.sh
@@ -0,0 +1,19 @@
+sudo apt -y install graphviz
+
+sudo apt -y install python3 python3-pip
+pip3 install pandas
+
+# Download Apache Jena and add arq to PATH
+jena_version=3.17.0
+jena_version_name=apache-jena-${jena_version}
+jena_archive=${jena_version_name}.tar.gz
+
+cd ${HOME}
+wget http://archive.apache.org/dist/jena/binaries/${jena_archive}
+tar xvvf ${jena_archive}
+rm ${jena_archive}
+ 
+repro.setenv JENA_HOME ${HOME}/${jena_version_name}
+repro.setenv JENA_CLASSPATH '${JENA_HOME}/lib/*'
+
+repro.prefixpath '${JENA_HOME}/bin'
--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ COPY .repro .repro
 USER repro
 
 # install required repro modules
-RUN repro.require geist exported --dev
+RUN repro.require geist exported --dev --demo
 RUN repro.require blazegraph-service 0.2.6 ${CIRSS_RELEASE}
 
 RUN repro.atstart start-blazegraph
